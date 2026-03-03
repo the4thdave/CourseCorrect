3d94dcac6a880c352de2100eccb65186cb4cf00f
David Munoz <7dmunoz@gmail.com>
2026-03-03 11:43:16 -0700

    Add basic frontend

---
+++ frontend/build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    application
+    id("org.springframework.boot") version "4.0.3"
+    id("io.spring.dependency-management") version "1.1.7"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(25)
+    }
+}
